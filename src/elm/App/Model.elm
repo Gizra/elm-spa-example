7fae7d6eba66cb900f899a22f528e7fa0eedd48c
Amitai Burstein <amitai@gizra.com>
2016-06-10 21:54:31 +0300

    Add App.Model

---
+++ src/elm/App/Model.elm
@@ -0,0 +1,22 @@
+module App.Model exposing(..)
+
+
+type Page
+  = Article
+  | Login
+  | PageNotFound
+
+
+type alias Model =
+  { activePage : Page
+  -- , article : Article.Model
+  -- If the user is anonymous, we want to know where to redirect them.
+  , nextPage : Maybe Page
+  }
+
+initialModel : Model
+initialModel =
+  { activePage = Login
+  -- , article = Article.initialModel
+  , nextPage = Nothing
+  }
